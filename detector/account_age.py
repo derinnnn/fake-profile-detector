889b690aa677c09f7fdedef08809e9f77b476da3
Benjamin Lawani <benjaminlawani@Benjamins-MacBook-Pro.local>
2025-05-14 23:45:26 +0100

    deleted

--- detector/account_age.py
+++
@@ -1,6 +0,0 @@
-# detector/account_age.py
-from datetime import datetime
-
-def is_account_new(date_string):
-    """Placeholder - currently disabled since we can't reliably get creation date"""
-    return False
--- detector/profile_picture.py
+++
@@ -1,2 +0,0 @@
-def has_profile_picture(img_url):
-    return "default_profile_images" not in img_url  # Modify if necessary
